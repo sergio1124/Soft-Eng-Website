e1c4f7ffebef2f770f484d64975733bbd08877c6
sergio1124 <mendoza.sergio1124@yahoo.com>
2019-11-27 08:25:27 -0600

    Finished the backend but have not tested the APIs routes with Postman

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: false
+  }
+});
+
+module.exports = User = mongoose.model("users", UserSchema);
