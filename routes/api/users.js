41c780b31cf7347caa45feec9e3634e614c2b0b1
sergio1124 <mendoza.sergio1124@yahoo.com>
2019-11-28 15:50:20 -0600

    build register and login pages and connected it to the back-end but it wont work yet. need to debug it

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,11 +3,9 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
-
 // Load input validation
 const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");
-
 // Load User model
 const User = require("../../models/User");
 
@@ -16,24 +14,20 @@ const User = require("../../models/User");
 // @access Public
 router.post("/register", (req, res) => {
   // Form validation
-
   const { errors, isValid } = validateRegisterInput(req.body);
-
   // Check validation
   if (!isValid) {
     return res.status(400).json(errors);
   }
-
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       return res.status(400).json({ email: "Email already exists" });
     } else {
       const newUser = new User({
+        name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
-        name: req.body.name
+        password: req.body.password
       });
-
       // Hash password before saving in database
       bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(newUser.password, salt, (err, hash) => {
@@ -81,7 +75,7 @@ router.post("/login", (req, res) => {
           payload,
           keys.secretOrKey,
           {
-            expiresIn: 31556926 // 1 year in seconds
+            expiresIn: 60 // 1 year in seconds
           },
           (err, token) => {
             res.json({
